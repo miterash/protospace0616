10b50c8c98754ad33bb1b55468c1c62d5c991919
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-06-30 15:34:01 +0900

    Add update(更新機能追加)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit]
+  before_action :set_prototype, only: [:show, :edit, :update]
 
   def index
     @prototypes = Prototype.order("created_at DESC").page(params[:page]).per(5)
@@ -23,6 +23,14 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototypes_path, notice: '編集デキタ~~~~~~~~~~~~！'
+    else
+      render :edit
+    end
   end
 
   private
