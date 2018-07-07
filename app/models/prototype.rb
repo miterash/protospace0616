84643cd34dbe27d374d38d19f32689cacc4e302b
miteo <s.miterashi@fitsplus.co.jp>
2018-07-07 14:52:33 +0900

    カテゴリータグ更新処理（prototype.rb編集）

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -23,4 +23,22 @@ class Prototype < ActiveRecord::Base
   def posted_date
     created_at.strftime('%b %d %a')
   end
+
+  def save_categories(tags)
+    current_tags = self.categories.pluck(:name) unless self.categories.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    # Destroy old taggings:
+    old_tags.each do |old_name|
+      self.categories.delete Category.find_by(name:old_name)
+    end
+
+    # Create new taggings:
+    new_tags.each do |new_name|
+      article_category = Category.find_or_create_by(name:new_name)
+      self.categories << article_category
+    end
+  end
+
 end
