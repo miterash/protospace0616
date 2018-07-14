f47a59a027e53b889f323009297ca0776c09ad61
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-07-14 14:40:39 +0900

    ボタンJS(テスト)

---
+++ app/assets/javascripts/button.js
@@ -0,0 +1,16 @@
+// $(function() {
+//   $('#dropdownMenu').on("click", function(){
+
+//     var html = `
+//                 <ul>
+//                   <li>
+//                     <a href="/prototypes/#{@prototype.id}/edit">edit</a>
+//                   </li>
+//                   <li>
+//                     <a rel="nofollow" data-method="delete">Delete</a>
+//                   </li>
+//                 </ul>
+//               `
+//     $('.btn.btn-default.dropdown-toggle').append(html);
+//   });
+// });
