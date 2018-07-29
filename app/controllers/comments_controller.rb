56de71c0981735559877aea5d47387db0c9813c6
keisuke koshikawa <0421kossy0421@gmail.com>
2018-07-29 13:11:31 +0900

    アクション、ルーティングの定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,17 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+  end
 
+  def create
+    @comment = Comment.new(comment_params)
+  end
+
+
+
+  private
+  def comment_params
+      params.require(:comment).permit(:content).merge(:user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create, :new, :edit, :update, :destroy]
+  end
 end
