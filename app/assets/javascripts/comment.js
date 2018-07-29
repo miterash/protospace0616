4325ee511c66ea5914da2b28988c371fe3bdffc5
keisuke koshikawa <0421kossy0421@gmail.com>
2018-07-29 14:31:01 +0900

    commentの非同期通信の実装(未完成)

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,39 @@
+$(document).on("turbolinks:load", function() {
+  function buildHTML(comment) {
+    var html = `<div class="comment__body">
+                  <h5 class="user-name">
+                    ${comment.user_name}
+                    <span class="posted-time">
+                      ${comment.created_at}
+                    </span>
+                  </h5>
+                  <p class="comment-body">
+                    ${comment.content}
+                  </p>
+                </div>`;
+    return html;
+  }
+
+  $('#new_comment').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.comment__content').append(html)
+      $('.comment textarea').val('')
+      $('.comment input').prop("disabled", false)
+    })
+    .fail(function() {
+      alert('エラーが発生しました');
+    })
+  })
+})
